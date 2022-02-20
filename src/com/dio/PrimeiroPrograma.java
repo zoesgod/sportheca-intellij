2ef7cfb2653517013dfc5851c28c55f7bb7db0ca
Zoes God <zoegod@pentest.zoegod>
2022-02-20 15:28:28 -0300

    iniciando projeto intellij

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*        int a = 5;
+        int b = 2;
+        System.out.println("Hello World " + (a+b));*/
+    }
+}
