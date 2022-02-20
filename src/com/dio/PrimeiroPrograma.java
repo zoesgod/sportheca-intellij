9e450fa246c41fee44814308471003d363797d85
zoegsod <roseanaheitor@gmail.com>
2022-02-20 16:41:19 -0300

    Adicionando classe livro

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -7,8 +7,42 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-        /*        int a = 5;
-        int b = 2;
-        System.out.println("Hello World " + (a+b));*/
+        Livro livro = new Livro("O Livro do Amor", 300);
+        System.out.println(livro);
+
     }
 }
+    class Livro {
+        private String nome;
+        private Integer numPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
+    }
+
